3a4c73c0ec8c61dbebf53a944a9456c3227bd365
Otto <105565584+GreenTomato5@users.noreply.github.com>
2024-11-13 16:59:04 -0600

    Updated Amp bar

--- src/main/java/frc/robot/subsystems/AmpBar.java
+++ src/main/java/frc/robot/subsystems/AmpBar.java
@@ -5,23 +5,32 @@ import com.revrobotics.CANSparkMax;
 
 import frc.robot.Constants;
 
-enum Ampstates {
+enum AmpStates {
     OUT,
     IN 
 }  
 
 public class AmpBar {
+    private AmpStates state;
     TalonFX rotationalMotor;
     CANSparkMax lPivotMotor;
     CANSparkMax rPivotMotor;
 
     public AmpBar() {
         rotationalMotor = new TalonFX(Constants.AmpBar.ROTATIONAL_MOTOR_ID);
-        //lPivotMotor = new CANSparkMax();
-        //rPivotMotor = new CANSparkMax();
+        lPivotMotor = new CANSparkMax(Constants.AmpBar.LPIVOT_MOTOR_ID);
+        rPivotMotor = new CANSparkMax(Constants.AmpBar.RPIVOT_MOTOR_ID);
+        state = AmpStates.IN;
+    }
+    public void setState(AmpStates state) { 
+        this.state = state;
+    }
+    public void periodic() {
+        if (state == AmpStates.IN) {
+            lPivotMotor.set(0);
+            rPivotMotor.set(0);
+            rotationalMotor.set(0);
+        }
     }
 
-     
-
-    
 }
